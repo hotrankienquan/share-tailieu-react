a2ccd8f395d79dd7b4947f54dc27f023339933b8
KienQuan <quan.htk>
2023-12-02 12:32:55 +0700

    first

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      Ai rồi cùng phải đổi thay
+      <p>Mặc cho tất cả những thứ ta đã vun đắp lâu nay</p>
+      <p>Kẻ ôm vấn vương</p>
+    </div>
+  );
+}
+
+export default Home;
